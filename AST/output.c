bbebb7c73fce3002da81660e58f47966c67da6c2
saioni <saionichatterjee1999@gmail.com>
2020-04-12 11:13:13 +0530

    test

--- AST/output.c
+++ AST/output.c
@@ -1,17 +1,14 @@
+
 int main(){
 	
 	int i, a, b;
 	int nume=3.45;	
-	for(i = 0;  i < 10; i++){
-		
-			b = 25 + i;
-		
+	
+	if(i<5){
+	a=a+5;
 	}
-	i=1;
-	while(i<5)
-	{
-		a=b+1;
-	}
-	if(i>1)
-		a=b+1;
+	
+
+	a = 3*2+5/4-9;
+
 }
